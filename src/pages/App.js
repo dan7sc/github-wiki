1cad37772b5aacf207d8ed8bf6db752b9c651768
dan7sc <dan7sc@users.noreply.github.com>
2024-08-22 15:33:35 -0300

    add Button component

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,5 @@
 import gitLogo from '../assets/github.png';
+import Button from '../components/Button';
 import Input from '../components/Input';
 import { Container } from './styles';
 
@@ -8,6 +9,7 @@ function App() {
     <Container>
       <img src={gitLogo} width={72} height={72} alt="github logo"/>
       <Input />
+      <Button />
     </Container>
   );
 }
